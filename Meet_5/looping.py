5ab7e53c77768817196972a6aff3cb39c34c3604
MKINGUEU <juliushtb9@student.esaunggul.ac.id>
2022-10-17 13:28:45 +0700

    meet-5

---
+++ Meet_5/looping.py
@@ -0,0 +1,14 @@
+for var in range(3):
+    print (var)
+
+for a in "pemograman":
+    if a=="p":
+        continue
+    if a=="g":
+        continue
+        # break
+    print (a)
+print("end")
+
+a = "pemograman"
+print(len(a))
