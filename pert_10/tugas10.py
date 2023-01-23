613013121ad256c2cb3ffcb87bd46ea681f4a217
MKINGUEU <juliushtb9@student.esaunggul.ac.id>
2023-01-23 17:24:17 +0700

    uas

---
+++ pert_10/tugas10.py
@@ -0,0 +1,10 @@
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+zpoints = np.array([0, 300])
+plt.plot(xpoints, ypoints, zpoints)
+plt.show()
+# %%
---
+++ sesi_8_array/contoh_array.py
@@ -0,0 +1,12 @@
+kendaraan = ["mobil", "motor", "sepeda", "becak"]
+kendaraan[3] = "kapal" #untuk mengubah urutan ke 3 yang tadinya becak jadi kapal
+
+kendaraan1 = ["mobil", "motor", "sepeda", "becak"]
+x = kendaraan1 [0] # untuk menampilkan urutan 0
+
+kendaraan2 = ["mobil", "motor", "sepeda", "becak"]
+y = len (kendaraan2)#menentukan jumlah kendaraan
+
+print (kendaraan)
+print (x)
+print (y)
